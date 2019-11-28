f0b4dd41a1a9527e21ca54edab85115ccc844e9a
Aritra <aritra119.26@gmail.com>
2019-11-28 20:34:02 +0530

    Added sample test case for route testing

---
+++ __tests__/routes/todo.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { setupServer } = require('../../src/server');
+
+describe('GET /todo', () => {
+  let server;
+
+  beforeEach(async () => {
+    server = await setupServer();
+  });
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  it('responds with 200', async () => {
+    const response = await server.inject({
+      method: 'get',
+      url: '/todo'
+    });
+    const todos = JSON.parse(response.payload);
+    expect(response.statusCode).to.equal(200);
+    expect(todos.length).to.equal(3);
+  });
+});
