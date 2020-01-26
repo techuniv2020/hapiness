88eefe4e9a79d4fe57113a05c2bab8cc215960cc
Aritra <aritra119.26@gmail.com>
2020-01-26 12:06:25 +0530

    Renamed fspromise import

--- src/utils/jsonFileHelper.js
+++ src/utils/jsonFileHelper.js
@@ -1,13 +1,13 @@
-const fsPromise = require('promise-fs');
+const fs = require('promise-fs');
 
 const readFromJsonFile = async (jsonPath) => {
-  const todosJson = await fsPromise.readFile(jsonPath, {encoding: 'utf-8'});
+  const todosJson = await fs.readFile(jsonPath, {encoding: 'utf-8'});
   return JSON.parse(todosJson);
 };
 
 const writeToJsonFile = async (jsonPath, updatedJson) => {
   const jsonString = JSON.stringify(updatedJson);
-  await fsPromise.writeFile(jsonPath, jsonString);
+  await fs.writeFile(jsonPath, jsonString);
 };
 
 module.exports = {
